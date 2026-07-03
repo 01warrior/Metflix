e9dedad9cbb89a56d03217a8166750b44b920467
Z User <z@container>
2026-07-03 05:26:52 +0000

    7dcc4d9a-0f5b-4a6a-a6c3-d111d351fccf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -720,7 +720,7 @@ function HeroSection() {
       {/* Gradients */}
       <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-transparent" />
       <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-[#0a0a0a] via-transparent to-transparent" />
-      <div className="absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-black/30 to-transparent pointer-events-none" />
+      <div className="absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-black/60 via-black/30 to-transparent pointer-events-none" />
 
       {/* Content */}
       <div className="relative z-10 flex flex-col justify-end h-full max-w-7xl mx-auto px-4 md:px-8 pb-12 md:pb-16">
